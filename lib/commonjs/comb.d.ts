6235fbe015ed77d7304497b5c0348fa533219cbe
Vladimir Pavković <vladimirpavk@gmail.com>
2021-03-16 13:00:06 +0100

    +

---
+++ lib/commonjs/comb.d.ts
@@ -0,0 +1,5 @@
+declare module "js-combperm"
+export function heapPerm(anyArray:any[]):any[];
+export function heapPermIterative(anyArray:any[]):any[];
+export function combAll(anyArray:any[], direction:string):any[];
+export function combAllStrict(anyArray:any[], direction:string, length:number, strict:boolean):any[];
